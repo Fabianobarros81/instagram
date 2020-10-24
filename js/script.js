13cc534c367e65106fc93f724ad0fdaa221c0358
Fabianobarros81 <barrosfabiano@gmail.com>
2020-10-24 02:56:50 -0300

    Commit Incial

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var i = 0;
+var imag = ['img/mob_fundo01.jpg','img/mob_fundo02.jpg','img/mob_fundo03.jpg','img/mob_fundo04.jpg','img/mob_fundo05.jpg'];
+
+function img01 (){
+
+    // var w = 0;
+    // while (w > 0) {
+       
+    	if ( i < imag.length) {
+	
+    		document.getElementById("trocarimg").src=imag[i];
+            i++;
+            // setTimeout(img01, 10000);
+	    }else {
+		    i = 0;
+		    document.getElementById("trocarimg").src=imag[i];
+            i++;
+            // setTimeout(img01, 10000);
+        }
+        setTimeout(img01, 10000);
+        //w++;
+    // };
+}
+setTimeout(img01, 0);
